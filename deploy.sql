7d469caa523344d3881a379ec1fec084f2c7ff96
AJABHIMANYU <63732138+AJABHIMANYU@users.noreply.github.com>
2025-10-02 14:35:15 +0530

    Create deploy.sql

---
+++ deploy.sql
@@ -0,0 +1,12 @@
+-- FILE: deploy.sql
+-- Deploy all database objects
+@tables.sql
+@trigger.sql
+@simple_employee_mgmt_pkg.pks
+@simple_employee_mgmt_pkg.pkb
+@department_mgmt_pkg.pks
+@department_mgmt_pkg.pkb
+@project_mgmt_pkg.pks
+@project_mgmt_pkg.pkb
+@utility.prc
+@utility.fnc
